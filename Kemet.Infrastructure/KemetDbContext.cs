b13908a76afa0c902fc14c7422b1f161ac8af9d7
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-04-03 02:06:42 +0200

    Add initial project structure and domain entities for Kemet

---
+++ Kemet.Infrastructure/KemetDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Kemet.Infrastructure;
+
+public class KemetDbContext : IdentityDbContext<User,Role, int> 
+{
+    private readonly IConfiguration _configuration;
+
+    public KemetDbContext(DbContextOptions<KemetDbContext> options, IConfiguration configuration) : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql(_configuration.GetConnectionString("KemetDb"));
+    }
+}
+
