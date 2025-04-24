4b0d6ebe8ed4804ac780268c88704606ae56cb04
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-04-24 18:07:43 +0200

    Adds custom exception classes
    Adds `InvalidPriceException` and `NotAvailableException`
    for handling specific error conditions in the application.
    Also, ensures that `AlreadyExistException` has a newline at end of file.

---
+++ Kemet.Application/Execptions/InvalidPriceException.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.Exceptions;
+
+public class InvalidPriceException : System.Exception
+{
+    public InvalidPriceException()
+    {
+    }
+
+    public InvalidPriceException(string? message) : base(message)
+    {
+    }
+
+    public InvalidPriceException(string? message, System.Exception? innerException) : base(message, innerException)
+    {
+    }
+}
