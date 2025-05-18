0f2768587e130980133e61c5b29c17940cc3f5b3
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-05-18 18:36:07 +0300

    Add MkCert Package

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-
+import mkcert from 'vite-plugin-mkcert'
 // https://vite.dev/config/
 export default defineConfig({
   server:{port: 3000},
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(), mkcert()],
 })
