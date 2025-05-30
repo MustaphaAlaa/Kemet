ce8fcdcde7871f1e46757046271501c50e034e66
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-05-30 03:14:48 +0300

    fix the styles from FirstTimeForm and NotFirstTimeForm Components, and add InputNumber component instead of using InputText for numbers

--- src/app/Components/CustomerInfo/FirstTimeForm.tsx
+++ src/app/Components/CustomerInfo/FirstTimeForm.tsx
@@ -1,9 +1,14 @@
 import { useState, type ChangeEvent, type FormEvent } from 'react';
 import InputText from '../ReuseableComponents/InputText';
+import type { sisi } from '../../layout/App';
+import InputNumber from '../ReuseableComponents/InputNumber';
 
-export default function FirstTimeForm() {
+export default function FirstTimeForm({ formStyle }: { formStyle: string }) {
 
     const [value, setValue] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+
 
     function handleSubmit(event: FormEvent<HTMLFormElement>): void {
         event.preventDefault();
@@ -12,31 +17,44 @@ export default function FirstTimeForm() {
         console.log("form is submitted")
     }
 
-    function handelChange(event: ChangeEvent<HTMLInputElement>): void {
+ 
+    const marginY = 'my-4'
+    const marginB = 'mb-2';
+    const md = 'md:flex-row md:justify-between';
 
-        // const val = event.target.value;
-        // setValue(val.trimStart());
-
-    }
+    const sharedStyle = `${marginY}  flex flex-col md:flex-row md:justify-between  items-center`;
 
 
     return (
-        <form onSubmit={handleSubmit} className='bg-sky-100 p-3 shadow '>
-            <div className=' m-2  flex     items-center '>
-                <label htmlFor="FirstName" className='w-32'> الاسم الاول</label>
-                <InputText name="FirstName" id="FirstName" onChange={handelChange}  ></InputText>
+        <form onSubmit={handleSubmit} className={formStyle}>
+            <div className={`${sharedStyle}`}>
+                <label htmlFor="FirstName" className={`${marginB} mx-3`}> الاسم الاول</label>
+                
+                <InputText name="FirstName" id="FirstName"  styles='rounded-md'  ></InputText>
             </div>
-            <div className=' m-2  flex     items-center '>
-                <label htmlFor="LastName" className='w-32'> الاسم الثاني</label>
-                <InputText name="LastName" id="LastName" onChange={handelChange}  ></InputText>
+            <div className={`${sharedStyle}`}>
+                <label htmlFor="LastName" className={`${marginB} mx-3`}> الاسم الثاني</label>
+                <InputText name="LastName" id="LastName"  styles='shrink rounded-md'  ></InputText>
             </div>
-            <div className=' m-2  flex     items-center '>
-                <label htmlFor="Address" className='w-32'> العنوان</label>
-                <InputText name="Address" id="Address" onChange={handelChange}  ></InputText>
+            <div className='flex flex-col @md:flex-row'>
+                <div className={` ${marginY}   flex flex-row items-center justify-around `}>
+                    <label htmlFor="Address" className={`${marginB}`}> المحافظة</label>
+                    {/* <InputText name="Address" id="Address"   ></InputText> */}
+                    <select className='bg-white p-3 rounded-xl cursor-pointer text-indigo-800'>
+                        <option value="" selected>-- أختار المحافظة --</option>
+
+                    </select>
+                </div>
+                {/* <div className={` ${marginY}  flex flex-col  sm:flex-col md:flex-row md:justify-around items-center  `}> */}
+                <div className={` ${sharedStyle}`}>
+                    <label htmlFor="Address" className={`${marginB}`}> العنوان</label>
+                    <InputText name="Address" id="Address"  styles='rounded-md' ></InputText>
+                </div>
             </div>
-            <div className=' m-2  flex     items-center '>
-                <label htmlFor="PhoneNumber" className='w-32'> رقم الموبايل</label>
-                <InputText name="PhoneNumber" id="PhoneNumber" number  ></InputText>
+            {/* <div className={` ${marginY}  flex flex-col md:flex-row items-center  `}> */}
+            <div className={` ${sharedStyle}`}>
+                <label htmlFor="PhoneNumber" className={`${marginB}`}> رقم الموبايل</label>
+                <InputNumber placeholder="01*********" name="phoneNumber" id="PhoneNumber" val={setPhoneNumber} maxLength={11} styles='rounded-md'  ></InputNumber>
             </div>
         </form>
     )
--- src/app/Components/CustomerInfo/NotFirstTimeForm.tsx
+++ src/app/Components/CustomerInfo/NotFirstTimeForm.tsx
@@ -1,28 +1,67 @@
-import type { ChangeEvent, FormEvent } from 'react';
+import { useState, type FormEvent } from 'react';
 import InputText from '../ReuseableComponents/InputText';
+import type { APIResponse } from '../../Models/APIResponse';
+import fetchFromUrl from '../../../APIFunctions/fetchFromUrl';
+import InputNumber from '../ReuseableComponents/InputNumber';
 
-export default function NotFirstTimeForm() {
-  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
+
+
+
+
+let previousNumber = '';
+
+export default function NotFirstTimeForm({formStyle}:{formStyle:string}) {
+
+    const [response, setResponse] = useState<APIResponse<boolean> | null>();
+    const [phoneNumber, setPhoneNumber] = useState<string>("");
+
+
+
+    function handleSubmit(event: FormEvent<HTMLFormElement>): void {
         event.preventDefault();
         console.log(event)
 
-        console.log("form is submitted")
+        console.log("form is submitted" + phoneNumber)
+
+
+        if (phoneNumber.length == 11) {
+            fetchFromUrl(`https://localhost:7048/api/Customer/Exist/${phoneNumber}`)
+                .then(d => setResponse(d));
+        }
+
     }
 
-    function handelChange(event: ChangeEvent<HTMLInputElement>): void {
-        if (event.target.value.length == 11)
-            console.log(event.target.value)
-    }
 
+    console.log(phoneNumber)
 
+    if(phoneNumber !== previousNumber && response !== null)
+            setResponse(null);
 
-    return (
-        <form onSubmit={handleSubmit} className='bg-sky-100 p-3 shadow '>
-            <div className='   flex     items-center '>
-                <label htmlFor="phoneNumber" className='w-32'> رقم الموبايل</label>
-                {/* <input type="text" name="phoneNumber" id="phoneNumber" onChange={handelChange} maxLength={11} className='shadow w-2xs  bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500' /> */}
-                <InputText name="phoneNumber" id="phoneNumber" onChange={handelChange} maxLength={11} styles='bg-red-500 text-sky-600  ' ></InputText>
-            </div>
-        </form>
-    )
+    previousNumber = phoneNumber;
+
+    return <>
+
+            <form method='get' onSubmit={handleSubmit} className={formStyle}>
+                <div className='   flex flex-col sm:flex-row justify-between gap-4    items-center m-1 '>
+                    <label htmlFor="phoneNumber" className='text-xl'> رقم الموبايل</label>
+                    <InputNumber  placeholder='XXX-XXX-XXX-XX' val={setPhoneNumber} name="phoneNumber" id="phoneNumber"   maxLength={11} styles='bg-red-500 text-sky-600  ' ></InputNumber>
+                </div>
+                {response &&
+
+                    <div className='flex flex-col shadow-lg/30 m-3 rounded-md'>
+                        {response.result ?
+                            <p className='border shadow  bg-green-100 text-green-800 p-3 text-xl rounded-md'> العميل موجود فعلًا </p>
+                            :
+                            <p className='border shadow  bg-red-100 text-red-500 p-3'>  العميل مش موجود اتاكد من الرقم او دخل بيانات جديدة   </p>
+                        }
+                    </div> 
+                }
+
+            </form>
+            {/* <p className=' bg-purple-100 sm-uppercase'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi minima saepe distinctio qui natus enim sit! Pariatur voluptate, a ipsum expedita quia ea ipsam praesentium iusto, ut repellat sit perspiciatis.</p> */}
+           
+          
+
+        </>
+     
 }
