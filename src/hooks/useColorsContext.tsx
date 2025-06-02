3112fa4838deff4cba9bf418433181244232e026
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-02 05:31:30 +0300

    delete Context that was written for colors only, refactor the useColorContext to work with the new generic context.

--- src/hooks/useColorsContext.tsx
+++ src/hooks/useColorsContext.tsx
@@ -1,38 +1,10 @@
-import { useContext, type JSX } from "react"
-import colorsContext from "../Contexts/colors";
-import type { APIResponse } from "../app/Models/APIResponse";
-import type { Color } from "../app/Models/Color";
+import { useContext } from "react"
+import { ColorContext } from "../Contexts/color/colorContext";
 
 
 
 
-type colorContextType = {
-    colors: APIResponse<Color[]> | undefined;
-    getColors: () => Promise<void>;
-    colorAdded: boolean;
-    colorUpdated: boolean;
-    deleteColor: (colorId: number) => Promise<void>;
-    colorDeleted: boolean;
-    createColor: ({ colorName, hexacode }: {
-        colorName: string;
-        hexacode: string;
-    }) => Promise<void>;
-    updateColor: ({ colorId, colorName, hexacode }: {
-        colorId: number;
-        colorName: string;
-        hexacode: string;
-    }) => Promise<void>;
-    updateState: boolean;
 
-    openUpdateForm: ({ color, setUpdateMode }: {
-        color: Color;
-        setUpdateMode: any;
-    }) => JSX.Element | undefined
-
-}
-
-
-
-export default function useColorsContext(): colorContextType {
-    return useContext(colorsContext);
+export default function useColorsContext() {
+    return useContext(ColorContext);
 }
