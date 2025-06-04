a7d29da5725aaa435000f6b3e186ad1732b683f5
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-04 17:15:45 +0300

    removes the ApiResponse constructor that was need  to create  new List<string> every call in the controllers, it deleted because not every request will have an error so i create a new List<string> whenever needed

---
+++ Kemet.Domain/Entities/ApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Entities;
+
+
+public class APIResponse
+{
+
+
+    public HttpStatusCode StatusCode { get; set; }
+
+    public bool IsSuccess { get; set; }
+
+    public List<string> ErrorMessages { get; set; }
+
+    public object Result { get; set; }
+}
