f23fdf41dc4e0e7989a7ff0ec85815f61a1b1588
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-04 09:41:16 +0300

    Add admina, and public controller for custmoer, admin controller contains Delete Functionailty for now, public controller contains Createing new Customer, checking if the customer is already exist

---
+++ Kemet.API/Controllers/Authenticated/Admin/CustomerAdminController.cs
@@ -0,0 +1,60 @@
+﻿using Entities;
+using Entities.Models.DTOs;
+using IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Kemet.API.Controllers.Authenticated.Admin;
+
+//a is for admin
+[Route("api/a/Customer/")]
+[ApiController]
+//[Authorize("admin")]
+public class CustomerAdminController : ControllerBase
+{
+    private ILogger<CustomerAdminController> _logger;
+    ICustomerService _customerService;
+    readonly APIResponse _response;
+    public CustomerAdminController(ILogger<CustomerAdminController> logger, ICustomerService customerService)
+    {
+        _logger = logger;
+        _customerService = customerService;
+
+        _response = new();
+    }
+
+    [HttpDelete("{phoneNumber}")]
+    public async Task<IActionResult> DeleteCustomer(string phoneNumber)
+    {
+
+        Console.WriteLine("YOu're in delete");
+
+
+        try
+        {
+
+            var c = new CustomerDeleteDTO { PhoneNumber = phoneNumber };
+            await _customerService.DeleteAsync(c);
+            await _customerService.SaveAsync();
+            var customer = await _customerService.RetrieveByAsync(c => c.PhoneNumber == phoneNumber);
+            _response.IsSuccess = true;
+            _response.ErrorMessages = null;
+            _response.StatusCode = customer is null ? HttpStatusCode.OK : HttpStatusCode.NotFound;
+            _response.Result = customer;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new() { ex.Message };
+            _response.StatusCode = HttpStatusCode.ExpectationFailed;
+            _response.Result = null;
+
+            return BadRequest();
+        }
+    }
+
+
+
+}
---
+++ Kemet.API/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using Entities;
+using Entities.API.Controllers;
+using Entities.Models;
+using Entities.Models.DTOs;
+using IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kemet.API.Controllers;
+
+
+
+[Route("api/Customer")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    private ILogger<CustomerController> _logger;
+    ICustomerService _customerService;
+    readonly APIResponse _response;
+    public CustomerController(ILogger<CustomerController> logger, ICustomerService customerService)
+    {
+        _logger = logger;
+        this._customerService = customerService;
+
+        _response = new();
+    }
+
+    [HttpGet("Exist/{phoneNumber}")]
+    public async Task<IActionResult> DoesCustomerExist(string phoneNumber)
+    {
+        try
+        {
+            var exist = await _customerService.IsCustomerExist(phoneNumber);
+            _response.IsSuccess = true;
+            _response.ErrorMessages = null;
+            _response.StatusCode = exist ? System.Net.HttpStatusCode.OK : System.Net.HttpStatusCode.NotFound;
+            _response.Result = exist;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest();
+        }
+    }
+
+    [HttpPost("Add")]
+    public async Task<IActionResult> AddCustomer([FromBody] CustomerCreateDTO createDTO)
+    {
+        try
+        {
+            var customer = await _customerService.CreateAsync(createDTO);
+            _response.IsSuccess = true;
+            _response.ErrorMessages = null;
+            _response.StatusCode = customer is not null ? System.Net.HttpStatusCode.OK : System.Net.HttpStatusCode.NotFound;
+            _response.Result = customer;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new();
+            _response.ErrorMessages?.Add(ex.Message);
+            _response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+            _response.Result = null;
+            return BadRequest(_response);
+        }
+    }
+}
