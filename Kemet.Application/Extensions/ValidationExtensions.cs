60f494547dee6e1256e615a5b2dca96fd3411426
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-04 17:04:17 +0300

    Add scope service for Product,  Customer, Address, Order, OrderItems, Size, Category, ProductVariant, Price, GovernorateDeliveryService validations and thier interfaces.

--- Kemet.Application/Extensions/ValidationExtensions.cs
+++ Kemet.Application/Extensions/ValidationExtensions.cs
@@ -10,7 +10,30 @@ public static partial class ApplicationLayerExtensions
     private static void AddValidations(this IServiceCollection service)
     {
         service.AddScoped<IColorValidation, ColorValidation>();
+
         service.AddScoped<IGovernorateValidation, GovernorateValidation>();
+
+        service.AddScoped<IGovernorateDeliveryValidation, GovernorateDeliveryValidation>();
+
+        service.AddScoped<ICustomerValidation, CustomerValidation>();
+
+        service.AddScoped<ISizeValidation, SizeValidation>();
+        service.AddScoped<IAddressValidation, AddressValidation>();
+
+        service.AddScoped<IPriceValidation, PriceValidation>();
+
+        service.AddScoped<IProductValidation, ProductValidation>();
+
+
+        service.AddScoped<IProductVariantValidation, ProductVariantValidation>();
+
+        service.AddScoped<IOrderValidation, OrderValidation>();
+
+        service.AddScoped<IOrderItemValidation, OrderItemValidation>();
+
+
+
+
         service.AddValidatorsFromAssemblyContaining<ColorCreateValidation>();
     }
 }
