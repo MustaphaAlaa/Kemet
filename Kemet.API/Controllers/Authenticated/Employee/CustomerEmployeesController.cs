5e14426bfa16ea021e5d0af3e6ab837ebe58c149
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-04 09:39:00 +0300

    Add admina, and public controller for size, admin controller contains CRUD functionality

---
+++ Kemet.API/Controllers/Authenticated/Employee/CustomerEmployeesController.cs
@@ -0,0 +1,69 @@
+﻿using Entities;
+using Entities.Models.DTOs;
+using IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Kemet.API.Controllers;
+
+//e is for employees
+[Route("api/e/Customer/")]
+[ApiController]
+//[Authorize("employee")]
+public class CustomerEmployeesController : ControllerBase
+{
+    private ILogger<CustomerEmployeesController> _logger;
+    ICustomerService _customerService;
+    readonly APIResponse _response;
+    public CustomerEmployeesController(ILogger<CustomerEmployeesController> logger, ICustomerService customerService)
+    {
+        _logger = logger;
+        this._customerService = customerService;
+
+        _response = new();
+    }
+
+    [HttpGet("All")]
+    public async Task<IActionResult> AllCustomers()
+    {
+        try
+        {
+            var allCustomers = await _customerService.RetrieveAllAsync();
+            //var exist = await _customerService.IsCustomerExist(phoneNumber);
+            _response.IsSuccess = true;
+            _response.ErrorMessages = null;
+            _response.StatusCode = allCustomers?.Count <= 0 ? System.Net.HttpStatusCode.OK : System.Net.HttpStatusCode.NotFound;
+            //_response.Result = exist;
+            _response.Result = allCustomers;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest();
+        }
+    }
+    [HttpGet("{phoneNumber}")]
+    //authorize emp or admin
+    public async Task<IActionResult> Customer(string phoneNumber)
+    {
+        try
+        {
+            var customer = await _customerService.FindCustomerByPhoneNumberAsync(phoneNumber);
+            _response.IsSuccess = true;
+            _response.StatusCode = customer is not null ? HttpStatusCode.OK : HttpStatusCode.NotFound;
+            _response.Result = customer;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new() { ex.Message };
+            _response.StatusCode = HttpStatusCode.ExpectationFailed;
+            _response.Result = null;
+
+            return BadRequest(_response);
+        }
+    }
+
+
+}
