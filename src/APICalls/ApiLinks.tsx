59e2d46e7519e5d5fba22a8991e7a1ead8402070
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-07 22:33:39 +0300

    Add ApiLinks object that contains Apis that used multiple time

---
+++ src/APICalls/ApiLinks.tsx
@@ -0,0 +1,10 @@
+
+import ApiDomain from "../app/Models/ApiDomain";
+
+export const ApiLinks = {
+    getAllCategories: `${ApiDomain}/api/category/index`,
+    getAllColors: `${ApiDomain}/api/color/index`,
+    getAllSizes: `${ApiDomain}/api/size/index`,
+}
+
+
