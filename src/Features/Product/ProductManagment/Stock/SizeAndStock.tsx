63468e967990e70443a37ea4a8fcbf7245b8957d
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-09 06:59:39 +0300

    Create LabelPanel and replace it with divs inside SizeAndStock component

--- src/Features/Product/ProductManagment/Stock/SizeAndStock.tsx
+++ src/Features/Product/ProductManagment/Stock/SizeAndStock.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import UpdateStock from "./UpdateStock";
 import { MdOutlineCreate } from "react-icons/md";
 import ApiLinks from "../../../../APICalls/ApiLinks";
+import LabelPanel from "../../../../Components/ReuseableComponents/LabelPanel";
 
 export default function SizeAndStock({
   size,
@@ -30,28 +31,10 @@ export default function SizeAndStock({
       `${ApiLinks.productVariant.details}/${productVariantId}/${selectedColor}/${size.sizeId}`
     );
 
-    let done = false;
-
-    if (data != undefined) {
       setProductVariantStock(data);
-      // stockQuantity = productVariantStock?.result?.stockQuantity;
-      console.log(data);
-      done = true;
-    } else {
-      console.log(`mmmmmmmmmooooooooooooooooooowFwwwwwwwwwwwwwwwwwwww`);
-    }
-
-    const ob = {
-      productVariantStock,
-      done: true,
-    };
-    console.log(done ? ob : "something wrong here");
+  
   };
   useEffect(() => {
-    // console.log(
-    //   `777777debug: pv.id => ${productVariantStock?.result?.productVariantId} color=> ${productVariantStock?.result?.color.colorId} size => ${productVariantStock?.result?.size.sizeId} stock=> ${productVariantStock?.result?.stockQuantity}`
-    // );
-
     getProductStockForSize();
   }, [selectedColor, size.sizeId, productVariantId]);
 
@@ -64,52 +47,46 @@ export default function SizeAndStock({
     <span className="">{stockQuantity ?? 0}</span>
   ) : (
     <UpdateStock
-      productVariant={productVariantStock.result}
+      productVariant={productVariantStock?.result}
       setStock={setStockQuantity}
       setUpdateMode={setUpdateMode}
     ></UpdateStock>
   );
-  // const content = !updateMode ? (
-  //   <div className="w-full bg-sky-300 border-white border-3 border-3 text-cyan-800  text-center text-3xl  p-3   rounded-b-md  font-bold  shadow-md/30">
-  //     <span className="">{stockQuantity ?? 0}</span>
-  //   </div>
-  // ) : (
-  //   <UpdateStock
-  //     productVariant={productVariantStock.result}
-  //     setStock={setStockQuantity}
-  //     setUpdateMode={setUpdateMode}
-  //   ></UpdateStock>
-  // );
 
   return (
     <>
       {productVariantStock ? (
         <div className="w-full   flex flex-col my-1 p-2 justify-between items-center rounded-xl bg-gradient-to-l from-cyan-700 to-sky-800 shadow-md/30 border-indigo-200 border border-3">
           <div className="flex flex-row items-center justify-between w-full">
-            {/* <SizeCircle size={size}></SizeCircle> */}
-            <div className="flex flex-col border w-1/3 border-3 border-sky-100 font-bold text-xl items-center rounded-lg bg-gray-300 text-white">
-              <span className="w-full text-center text-sky-800 p-1  bg-white">
-                المقاس
-              </span>
-              <span className="p-3 text-blue-800">{size.name}</span>
-            </div>
-            <div className="flex flex-col border w-1/3 border-3 border-sky-100  text-xl items-center rounded-lg bg-sky-300 text-white">
-              <div className="flex flex-row justify-between items-center p-1  w-full text-center text-sky-800   font-bold bg-white">
-                <span>المخزون</span>
-                <MdOutlineCreate
-                  onClick={handleClick}
-                  className="cursor-pointer bg-rose-500  rounded-sm  text-white"
-                />
-              </div>
-              <span className="p-3 text-blue-800 font-bold">{content}</span>
-            </div>
 
-            {/* <div className="text-center w-1/2 font-bold   text-center text-sky-800 p-1 font-bold  ">
-              <div className="w-full text-center text-sky-800 p-1 font-bold bg-white ">
-                المخزون
-              </div>
-              {content}
-            </div> */}
+
+            <LabelPanel
+              className="w-1/3"
+              defaultStyle
+              label={<span>المقاس</span>}
+            >
+              <span className="p-3 text-blue-800">{size.name}</span>
+            </LabelPanel>
+
+
+
+            <LabelPanel
+              styleLabel="flex flex-row justify-between items-center p-1  w-full text-center text-sky-800   font-bold bg-white"
+              className=" w-1/3 border-sky-100  items-center bg-sky-300"
+              label={
+                <>
+                  <span>المخزون</span>
+                  <MdOutlineCreate
+                    onClick={handleClick}
+                    className="cursor-pointer bg-rose-500  rounded-sm  text-white"
+                  />
+                </>
+              }
+            >
+              <span>{content}</span>
+            </LabelPanel>
+
+
           </div>
         </div>
       ) : null}
