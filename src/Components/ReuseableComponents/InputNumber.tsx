643969abe73dcedbe6f409671d385614b6dfdf3e
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-13 22:40:06 +0300

    fix styling and add initValue prop as option

--- src/Components/ReuseableComponents/InputNumber.tsx
+++ src/Components/ReuseableComponents/InputNumber.tsx
@@ -1,12 +1,27 @@
 import classNames from "classnames";
-import { useState, type ChangeEvent } from "react";
+import { useState, type ChangeEvent, type JSX } from "react";
 
-export default function InputNumber({ direction = "ltr", styles = "", val, placeholder = "", ...rest }) {
-    const classes = classNames('shadow w-2xs  bg-white focus:outline-none focus:ring-2 focus:ring-blue-300 p-2 text-sky-600  ', styles);
+export default function InputNumber({ initValue, defaultStyle = true , direction = "ltr", styles = "", val, placeholder = "", ...rest }: {
+    [x: string]: any;
+    initValue?:number;
+    defaultStyle?: boolean | undefined;
+    direction?: string | undefined;
+    styles?: string | undefined;
+    val: any;
+    placeholder?: string | undefined;
+}): JSX.Element {
+
+  
+
+    const classes = classNames('  w-2xs   focus:outline-none   p-2 ', {
+        'shadow      focus:ring-2 focus:ring-blue-300   ': defaultStyle,
+        'bg-white': styles.search('bg') < 0,
+        'text': styles.search('text') < 0,
+    }, styles);
 
 
 
-    const [value, setValue] = useState("");
+    const [value, setValue] = useState<number|string>(initValue ?? "");
 
 
     function handelChange(event: ChangeEvent<HTMLInputElement>): void {
@@ -15,7 +30,7 @@ export default function InputNumber({ direction = "ltr", styles = "", val, place
         const regex = /^\d*$/;
         if (regex.test(targetValue)) {
             setValue(targetValue)
-            val(targetValue); 
+            val(targetValue);
         }
     }
 
@@ -26,5 +41,5 @@ export default function InputNumber({ direction = "ltr", styles = "", val, place
         onChange: handelChange
     }
 
-    return <input    {...numberAttr}   {...rest} type="text" className={classes} />
+    return <input   {...numberAttr}   {...rest} type="text" className={classes} />
 };
