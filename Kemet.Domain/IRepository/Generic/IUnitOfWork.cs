227e770870e57c44af98b157519e0efe6ab6d3d9
MustaphaAlaa <MostafaAlaa11998@gmail.com>
2025-06-24 04:58:44 +0300

    Add Transiction to UnitOfWork

--- Kemet.Domain/IRepository/Generic/IUnitOfWork.cs
+++ Kemet.Domain/IRepository/Generic/IUnitOfWork.cs
@@ -5,4 +5,7 @@ public interface IUnitOfWork : IDisposable
     IBaseRepository<T> GetRepository<T>()
         where T : class;
     Task<int> SaveChangesAsync(); // Use async for best practice
+    Task BeginTransactionAsync();
+    Task CommitAsync();
+    Task RollbackAsync();
 }
