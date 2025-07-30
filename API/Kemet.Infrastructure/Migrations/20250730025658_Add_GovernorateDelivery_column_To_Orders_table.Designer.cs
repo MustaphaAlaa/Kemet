1fe8eef1cb6d95a2f2862a58982524c1492ee273
MustaphaAlaa <mostafaalaa11998@gmail.com>
2025-07-30 06:16:27 +0300

    Add GovernorateDeliveryId to Orders (Migration)

--- API/Kemet.Infrastructure/Migrations/20250730025658_Add_GovernorateDelivery_column_To_Orders_table.Designer.cs
+++ API/Kemet.Infrastructure/Migrations/20250730025658_Add_GovernorateDelivery_column_To_Orders_table.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Entities.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Kemet.Intrastructure.Migrations
 {
     [DbContext(typeof(KemetDbContext))]
-    partial class KemetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250730025658_Add_GovernorateDelivery_column_To_Orders_table")]
+    partial class Add_GovernorateDelivery_column_To_Orders_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ API/Kemet.Infrastructure/Migrations/20250730025658_Add_GovernorateDelivery_column_To_Orders_table.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kemet.Intrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_GovernorateDelivery_column_To_Orders_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GovernorateDeliveryId",
+                table: "Orders",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_GovernorateDeliveryId",
+                table: "Orders",
+                column: "GovernorateDeliveryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_GovernorateDelivery_GovernorateDeliveryId",
+                table: "Orders",
+                column: "GovernorateDeliveryId",
+                principalTable: "GovernorateDelivery",
+                principalColumn: "GovernorateDeliveryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_GovernorateDelivery_GovernorateDeliveryId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_GovernorateDeliveryId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "GovernorateDeliveryId",
+                table: "Orders");
+        }
+    }
+}
