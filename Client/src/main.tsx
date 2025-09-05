ee01c02c6e48fd199e7fe5b975cd7a6f69a21908
MustaphaAlaa <mostafaalaa11998@gmail.com>
2025-09-05 04:25:28 +0300

    change route filename to routes

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/route'
+import { router } from './routes/routes'
 import { Provider } from 'react-redux'
 import { store } from '../store/store'
 createRoot(document.getElementById('root')!).render(
