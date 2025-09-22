7b837a4c05497a4fc59466fbd42625e3f7714886
MustaphaAlaa <mostafaalaa11998@gmail.com>
2025-09-22 11:10:36 +0300

    Split  KemetDbContext into two classes, one for DbSets and other for methods, constructor, overloads

--- API/Kemet.Infrastructure/KemetDbContext.cs
+++ API/Kemet.Infrastructure/KemetDbContext.cs
@@ -5,43 +5,9 @@ using Microsoft.Extensions.Configuration;
 
 namespace Entities.Infrastructure;
 
-public class KemetDbContext : IdentityDbContext<User, Role, Guid>
+
+public partial class KemetDbContext : IdentityDbContext<User, Role, Guid>
 {
-    public DbSet<Address> Addresses { get; set; }
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Color> Colors { get; set; }
-    public DbSet<Customer> Customers { get; set; }
-    public DbSet<DeliveryCompany> DeliveryCompanies { get; set; }
-    public DbSet<DeliveryCostPayment> DeliveryCostPayments { get; set; }
-    public DbSet<DeliveryPaymentStatus> DeliveryPaymentStatuses { get; set; }
-    public DbSet<DeliveryTransaction> DeliveryTransactions { get; set; }
-    public DbSet<Governorate> Governorates { get; set; }
-    public DbSet<GovernorateDelivery> GovernorateDelivery { get; set; }
-    public DbSet<GovernorateDeliveryCompany> GovernorateDeliveryCompanies { get; set; }
-
-    // Order related entities
-    public DbSet<Order> Orders { get; set; }
-    public DbSet<OrderItem> OrderItems { get; set; }
-    public DbSet<OrderInvoice> OrderInvoices { get; set; }
-    public DbSet<OrderReceiptNote> OrderReceiptNotes { get; set; }
-    public DbSet<OrderReceiptStatus> OrderReceiptStatuses { get; set; }
-    public DbSet<OrderStatus> OrderStatuses { get; set; }
-
-    public DbSet<Payment> Payments { get; set; }
-    public DbSet<PaymentType> PaymentTypes { get; set; }
-
-    public DbSet<Price> Prices { get; set; }
-    public DbSet<Product> Products { get; set; }
-    public DbSet<ProductQuantityPrice> ProductQuantityPrices { get; set; }
-
-    public DbSet<ProductVariant> ProductVariants { get; set; }
-
-    public DbSet<Return> Returns { get; set; }
-    public DbSet<ReturnItem> ReturnItems { get; set; }
-    public DbSet<ReturnStatus> ReturnStatuses { get; set; }
-
-    public DbSet<Size> Sizes { get; set; }
-
     private readonly IConfiguration _configuration;
 
     public KemetDbContext(DbContextOptions<KemetDbContext> options, IConfiguration configuration)
@@ -60,7 +26,5 @@ public class KemetDbContext : IdentityDbContext<User, Role, Guid>
     {
         base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(KemetDbContext).Assembly);
-
- 
     }
 }
