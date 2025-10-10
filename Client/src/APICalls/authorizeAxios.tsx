6965477a376a7d40178d591045e2a2a219b4df51
MustaphaAlaa <mostafaalaa11998@gmail.com>
2025-10-11 01:43:21 +0300

    rename privateApi to authorizeAxios

--- Client/src/APICalls/authorizeAxios.tsx
+++ Client/src/APICalls/authorizeAxios.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const privateApi = axios.create();
+const authorizeAxios = axios.create();
 
 
-privateApi.interceptors.request.use((config) => {
+authorizeAxios.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -11,4 +11,4 @@ privateApi.interceptors.request.use((config) => {
     return config;
 })
 
-export { privateApi };
+export { authorizeAxios };
